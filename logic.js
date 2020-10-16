02565adb6d38b67c3313819fb6ba8fcbd8cd9f12
shane9b3 <38891628+shane9b3@users.noreply.github.com>
2020-10-16 11:16:04 -0400

    Control panel set up

--- logic.js
+++ logic.js
@@ -2,6 +2,66 @@
 
     const ws = new WebSocket('ws://localhost:49122');
 
+    
+    function orangeWins() {
+      var OW = localStorage.getItem("OrangeWins")
+      if (OW === '0'){
+        $('#orange1').addClass('invisible');
+        $('#orange2').addClass('invisible');
+        $('#orange3').addClass('invisible');
+        $('#orange4').addClass('invisible');
+      }else if (OW === '1') {
+        $('#orange1').removeClass('invisible');
+        $('#orange2').addClass('invisible');
+        $('#orange3').addClass('invisible');
+        $('#orange4').addClass('invisible');
+      }else if (OW === '2') {
+        $('#orange1').removeClass('invisible');
+        $('#orange2').removeClass('invisible');
+        $('#orange3').addClass('invisible');
+        $('#orange4').addClass('invisible');
+      }else if (OW === '3') {
+        $('#orange1').removeClass('invisible');
+        $('#orange2').removeClass('invisible');
+        $('#orange3').removeClass('invisible');
+        $('#orange4').addClass('invisible');
+      }else if (OW === '4') {
+        $('#orange1').removeClass('invisible');
+        $('#orange2').removeClass('invisible');
+        $('#orange3').removeClass('invisible');
+        $('#orange4').removeClass('invisible'); 
+      }
+    }
+
+    function blueWins() {
+      var OW = localStorage.getItem("BlueWins")
+      if (OW === '0'){
+        $('#blue1').addClass('invisible');
+        $('#blue2').addClass('invisible');
+        $('#blue3').addClass('invisible');
+        $('#blue4').addClass('invisible');
+      }else if (OW === '1') {
+        $('#blue1').removeClass('invisible');
+        $('#blue2').addClass('invisible');
+        $('#blue3').addClass('invisible');
+        $('#blue4').addClass('invisible');
+      }else if (OW === '2') {
+        $('#blue1').removeClass('invisible');
+        $('#blue2').removeClass('invisible');
+        $('#blue3').addClass('invisible');
+        $('#blue4').addClass('invisible');
+      }else if (OW === '3') {
+        $('#blue1').removeClass('invisible');
+        $('#blue2').removeClass('invisible');
+        $('#blue3').removeClass('invisible');
+        $('#blue4').addClass('invisible');
+      }else if (OW === '4') {
+        $('#blue1').removeClass('invisible');
+        $('#blue2').removeClass('invisible');
+        $('#blue3').removeClass('invisible');
+        $('#blue4').removeClass('invisible');
+      }
+    }
 
     var blueN = []
     var blueF = []
